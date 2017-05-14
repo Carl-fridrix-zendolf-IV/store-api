13284f6ffec486c7d59050a24261128d123ba085
Egor Medvedev <social.neet@gmail.com>
2017-05-14 20:30:04 +0300

    Add skills as free auth method, bug fix

--- routes/public/services.public.js
+++ routes/public/services.public.js
@@ -29,4 +29,26 @@ module.exports = (app, routes) => {
      * @apiUse ResponseError
      */
     app.get('/api/public/v0/services/languages', routes.views.api.languagesList);
-}
+
+    /**
+     * @api {get} /api/public/v0/services/skills Get skills list
+     * @apiName Skills list
+     * @apiGroup Services
+     *
+     * @apiParam {String} [skip] Filter for skiping elements.
+     * @apiParam {String} [limit] Filter for limiting data array (Default: 999999).
+     *
+     * @apiUse ResponseSuccess
+     * @apiSuccess {String} total Total count of products.
+     * @apiSuccess {Object} data Data object
+     * @apiSuccess {String} data._id Category id.
+     * @apiSuccess {String} data.name Product name.
+     * @apiSuccess {String} data.price Product price.
+     * @apiSuccess {String} data.description Product description.
+     * @apiSuccess {String} data.image Link to product image.
+     * @apiSuccess {String} data.icon Link to product icon.
+     *
+     * @apiUse ResponseError
+     */
+    app.get('/api/public/v0/services/skills', routes.views.api.products);
+};
--- routes/public/store.public.js
+++ routes/public/store.public.js
@@ -18,28 +18,4 @@ module.exports = (app, routes) => {
      * @apiUse ResponseError
      */
     app.get('/api/public/v0/store/categories', routes.views.api.categories);
-
-    /**
-     * @api {get} /api/public/v0/store/skills Get skills list
-     * @apiName Skills list
-     * @apiGroup Store
-     *
-     * @apiHeader {String} Authorization User authorization token.
-     *
-     * @apiParam {String} [skip] Filter for skiping elements.
-     * @apiParam {String} [limit] Filter for limiting data array (Default: 999999).
-     *
-     * @apiUse ResponseSuccess
-     * @apiSuccess {String} total Total count of products.
-     * @apiSuccess {Object} data Data object
-     * @apiSuccess {String} data._id Category id.
-     * @apiSuccess {String} data.name Product name.
-     * @apiSuccess {String} data.price Product price.
-     * @apiSuccess {String} data.description Product description.
-     * @apiSuccess {String} data.image Link to product image.
-     * @apiSuccess {String} data.icon Link to product icon.
-     *
-     * @apiUse ResponseError
-     */
-    app.get('/api/public/v0/store/skills', routes.views.api.products);
 };
